59845861eabe372579e8161202d7cf0676119596
Jason Schrader <whoabuddy@users.noreply.github.com>
2024-10-03 13:35:49 -0700

    fix: add stacks auth button using connect

--- frontend-crew/src/components/auth/Authentication.tsx
+++ frontend-crew/src/components/auth/Authentication.tsx
@@ -1,9 +1,92 @@
 "use client";
 import React from "react";
 import { Button } from "@/components/ui/button";
-import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card";
 import { GithubIcon } from "lucide-react";
+import { toast } from "@/hooks/use-toast";
 import { socialAuth } from "@/lib/auth-helpers";
+import { AppConfig, showConnect, UserSession } from "@stacks/connect";
+
+const appConfig = new AppConfig(["store_write", "publish_data"]);
+export const userSession = new UserSession({ appConfig });
+
+function authenticateWallet() {
+  showConnect({
+    appDetails: {
+      name: "AIBTC Crew Generator",
+      icon: window.location.origin + "/logos/aibtcdev-avatar-1000px.png",
+    },
+    redirectTo: "/",
+    onFinish: () => {
+      toast({
+        title: "Wallet Connected",
+        description: "Successfully logged in with your wallet.",
+      });
+      window.location.reload();
+    },
+    userSession,
+  });
+}
+
+function disconnectWallet() {
+  userSession.signUserOut("/");
+}
+
+function truncateWalletAddress(address: string) {
+  return `${address.slice(0, 5)}...${address.slice(-5)}`;
+}
+
+function ConnectWallet() {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+
+  if (mounted && userSession.isUserSignedIn()) {
+    const mainnetAddress =
+      userSession.loadUserData().profile.stxAddress.mainnet;
+    const testnetAddress =
+      userSession.loadUserData().profile.stxAddress.testnet;
+    return (
+      <div className="Container">
+        <Card className="w-full max-w-md mx-auto text-center">
+          <CardHeader>
+            <CardTitle className="text-2xl font-bold">
+              Wallet Connected
+            </CardTitle>
+          </CardHeader>
+          <CardContent>
+            <p>mainnet: {truncateWalletAddress(mainnetAddress)}</p>
+            <p>testnet: {truncateWalletAddress(testnetAddress)}</p>
+          </CardContent>
+          <CardFooter>
+            <Button
+              variant="outline"
+              onClick={disconnectWallet}
+              className="w-full flex items-center justify-center"
+            >
+              Disconnect Wallet
+            </Button>
+          </CardFooter>
+        </Card>
+      </div>
+    );
+  }
+
+  return (
+    <Button
+      variant="outline"
+      onClick={authenticateWallet}
+      className="w-full flex items-center justify-center"
+    >
+      Connect Wallet
+    </Button>
+  );
+}
 
 export function Authentication() {
   return (
@@ -22,6 +105,7 @@ export function Authentication() {
           <GithubIcon className="mr-2 h-4 w-4" />
           Continue with Github
         </Button>
+        <ConnectWallet />
         {/* <Button
           variant="outline"
           onClick={() => socialAuth("google")}
